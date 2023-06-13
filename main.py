ff7553e92b6d4cdb1c4b64b96db2946a46f7e069
FabioGongOmonte <fabiogongomonte@gmail.com>
2023-06-13 02:10:37 -0400

    Initial Commit - Can mirror and stack videos

---
+++ main.py
@@ -0,0 +1,15 @@
+from moviepy.editor import VideoFileClip, vfx, clips_array
+
+bottom = VideoFileClip("bottom.mp4") # import the video that will be at the bottom
+mirrored_bottom = bottom.resize(0.5).fx(vfx.mirror_x) # mirror the video
+
+top = VideoFileClip("top.mp4").resize(0.5)      # import the video that will be at the top
+
+final_clip = clips_array([[top],
+                          [mirrored_bottom]])
+audio_top = top.audio
+
+final_clip = final_clip.set_audio(audio_top)
+
+final_clip.resize(width=1920).write_videofile("my_video.mp4")
+
